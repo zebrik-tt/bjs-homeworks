d687711cf741b7d697c41cba3392aedb892882ea
user1 <1k@1k.ru>
2023-02-08 20:42:03 +0300

    modified:   3.arrays/task.js. Задача 1-2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,29 @@
+// Задача 1
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length &&
+    arr1.every((item, index) => item === arr2[index]);
 }
 
+
+// Задача 2
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+  let result = 0;
+  // result = users.filter(...).map(...).reduce(...)
+  if (users.length > 0 && (gender === "мужской" || gender === "женский")) {
+
+    result = users.filter(
+      (item) => item.gender === gender).map(
+        (item) => item.age).reduce(
+          function (acc, item, index, arr) {
+            const amount = acc + item;
+            if (index === arr.length - 1) {
+              return amount / arr.length;
+            }
+
+            return amount;
+          }, 0);
+  }
+
+  return result;
 }
